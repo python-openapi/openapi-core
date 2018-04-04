56be4b10eb6dfa1020d451626a2f58f836f1729c
amcmanigal <amcmanigal@pindrop.com>
2018-04-04 10:15:45 -0400

    Fixing Subschema Required Properties Validation
    Currently if valid swagger syntax is used for model composition an
    error will be thrown due to the lack of a type property. This was
    corrected by making object the default type.
    
    schema_type = schema_deref.get('type', 'object')
    
    I changed the swagger definition to test for this. Now PetCreate is a
    composite of PetCreatePartOne and PetCreatePartTwo. However, this
    caused `test_post_pets_empty_body` to fail, which turned out to be a
    bug in the required properties.
    
    In `_unmarshal_object` the `get_all_properties` method is called to get
    all properties from the subschemas. However, this is not done for
    required properties, meaning that only top level required properties
    will be correctly validated. I have added a
    `get_all_required_properties’ to fix this.
    
    This caused `test_get_pets` to fail. In this case the bug allowed an
    incorrect test case to be introduced. Pet requires `id`, but it also
    requires name because it inherits from PetCreate. I have fixed this
    test case by adding the missing required property.
    
    After these changes `test_get_pet_not_found` failed due to a string
    formatting error (double quotes vs single quotes). I fixed this by
    switching to dictionary comparisons.

--- tests/integration/test_validators.py
+++ tests/integration/test_validators.py
@@ -245,6 +245,7 @@ class TestResponseValidator(object):
             'data': [
                 {
                     'id': 1,
+                    'name': 'Sparky'
                 },
             ],
         }
