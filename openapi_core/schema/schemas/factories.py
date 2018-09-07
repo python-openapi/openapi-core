8db5c08ed18938769fb54e6cb6ff8b07d1527a08
Correl Roush <correl@gmail.com>
2018-09-07 16:40:10 -0400

    Add support for additional validation properties
    Add support for the following validation properties:
    - multipleOf
    - maximum
    - exclusiveMaximum
    - minimum
    - exclusiveMinimum
    - maxLength
    - minLength
    - pattern
    - maxItems
    - minItems
    - uniqueItems
    - maxProperties
    - minProperties
    
    Fixes #49

--- openapi_core/schema/schemas/factories.py
+++ openapi_core/schema/schemas/factories.py
@@ -29,6 +29,19 @@ class SchemaFactory(object):
         all_of_spec = schema_deref.get('allOf', None)
         one_of_spec = schema_deref.get('oneOf', None)
         additional_properties_spec = schema_deref.get('additionalProperties')
+        min_items = schema_deref.get('minItems', None)
+        max_items = schema_deref.get('maxItems', None)
+        min_length = schema_deref.get('minLength', None)
+        max_length = schema_deref.get('maxLength', None)
+        pattern = schema_deref.get('pattern', None)
+        unique_items = schema_deref.get('uniqueItems', None)
+        minimum = schema_deref.get('minimum', None)
+        maximum = schema_deref.get('maximum', None)
+        multiple_of = schema_deref.get('multipleOf', None)
+        exclusive_minimum = schema_deref.get('exclusiveMinimum', False)
+        exclusive_maximum = schema_deref.get('exclusiveMaximum', False)
+        min_properties = schema_deref.get('minProperties', None)
+        max_properties = schema_deref.get('maxProperties', None)
 
         properties = None
         if properties_spec:
@@ -56,6 +69,12 @@ class SchemaFactory(object):
             default=default, nullable=nullable, enum=enum,
             deprecated=deprecated, all_of=all_of, one_of=one_of,
             additional_properties=additional_properties,
+            min_items=min_items, max_items=max_items, min_length=min_length,
+            max_length=max_length, pattern=pattern, unique_items=unique_items,
+            minimum=minimum, maximum=maximum, multiple_of=multiple_of,
+            exclusive_maximum=exclusive_maximum,
+            exclusive_minimum=exclusive_minimum,
+            min_properties=min_properties, max_properties=max_properties,
         )
 
     @property
