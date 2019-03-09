4456f580e89f538800071059253051b0823db927
Daniel Bradburn <daniel@crunchrapps.com>
2019-03-09 08:06:10 +0100

    added simple test that checks that each schema Exception type can be converted to a string without error

---
+++ tests/unit/schema/test_exceptions.py
@@ -0,0 +1,25 @@
+from openapi_core.schema.schemas import exceptions
+import pytest
+
+
+def is_open_api_exception(exception_type):
+    try:
+        return issubclass(exception_type, exceptions.OpenAPISchemaError)
+    except TypeError:
+        return False
+
+
+class TestExceptions:
+
+    @pytest.mark.parametrize(
+        "exception_type",
+        (
+            exception_type
+            for exception_type_name in dir(exceptions)
+            for exception_type in [getattr(exceptions, exception_type_name)]
+            if is_open_api_exception(exception_type)
+        )
+    )
+    def test_convert_to_string(self, exception_type):
+        # verify that we can convert to a string without error
+        str(exception_type)
