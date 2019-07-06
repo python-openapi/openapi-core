d26402711059f8e0c2b711126f64fd425a8ddba1
Pedro Peixoto <phrfpeixoto@gmail.com>
2019-07-06 16:18:15 -0300

    Adding test to expose failure

--- tests/unit/schema/test_schemas_registry.py
+++ tests/unit/schema/test_schemas_registry.py
@@ -4,10 +4,23 @@ from jsonschema.validators import RefResolver
 from openapi_spec_validator.validators import Dereferencer
 from openapi_spec_validator import default_handlers
 
+from openapi_core.schema.schemas.enums import SchemaType
 from openapi_core.schema.schemas.registries import SchemaRegistry
 
 
-class TestSchemaRegistryGetOrCreate(object):
+class BaseTestRegistry(object):
+
+    @pytest.fixture
+    def dereferencer(self, spec_dict):
+        spec_resolver = RefResolver('', spec_dict, handlers=default_handlers)
+        return Dereferencer(spec_resolver)
+
+    @pytest.fixture
+    def schemas_registry(self, dereferencer):
+        return SchemaRegistry(dereferencer)
+
+
+class TestSchemaRegistryGetOrCreate(BaseTestRegistry):
 
     @pytest.fixture
     def schema_dict(self):
@@ -33,17 +46,64 @@ class TestSchemaRegistryGetOrCreate(object):
             },
         }
 
-    @pytest.fixture
-    def dereferencer(self, spec_dict):
-        spec_resolver = RefResolver('', spec_dict, handlers=default_handlers)
-        return Dereferencer(spec_resolver)
-
-    @pytest.fixture
-    def schemas_registry(self, dereferencer):
-        return SchemaRegistry(dereferencer)
-
     def test_recursion(self, schemas_registry, schema_dict):
         schema, _ = schemas_registry.get_or_create(schema_dict)
 
         assert schema.properties['suberror'] ==\
             schema.properties['suberror'].properties['suberror']
+
+
+class TestAllOf(BaseTestRegistry):
+
+        @pytest.fixture
+        def schema_dict(self):
+            return {
+                'type': 'object',
+                'properties': {
+                    'message': {
+                        'type': 'string',
+                    },
+                    'suberror': {
+                        'allOf': [
+                            {'$ref': '#/components/schemas/Error'},
+                            {'$ref': '#/components/schemas/TS'}
+                        ]
+                    },
+                    'ts': {
+                        '$ref': '#/components/schemas/TS'
+                    }
+                },
+            }
+
+        @pytest.fixture
+        def spec_dict(self, schema_dict):
+            return {
+                'components': {
+                    'schemas': {
+                        'Error': {
+                            'type': 'object',
+                            'properties': {
+                                'message': {
+                                    'type': 'string'
+                                },
+                            }
+                        },
+                        'TS': {
+                            'type': 'object',
+                            'properties': {
+                                'moment': {
+                                    'type': 'string',
+                                    'format': 'datetime'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+
+        def test_all_of_intrinsic_type(self, schemas_registry, schema_dict):
+            schema, _ = schemas_registry.get_or_create(schema_dict)
+
+            suberror = schema.properties['suberror']
+            assert suberror.type == SchemaType.OBJECT
