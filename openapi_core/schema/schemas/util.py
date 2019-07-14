8260426b9177862ffa11558b82f556ba675077d7
Pedro Peixoto <phrfpeixoto@gmail.com>
2019-07-14 00:25:40 -0300

    Creating a decorator that warns the developer whenever he fails to set a context.

--- openapi_core/schema/schemas/util.py
+++ openapi_core/schema/schemas/util.py
@@ -3,6 +3,9 @@ from distutils.util import strtobool
 from six import string_types
 from json import dumps
 
+from openapi_core.schema.schemas.enums import UnmarshalContext
+from openapi_core.schema.schemas.exceptions import UnmarshalContextNotSet
+
 
 def forcebool(val):
     if isinstance(val, string_types):
