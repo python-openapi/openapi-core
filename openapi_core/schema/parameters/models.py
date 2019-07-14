975ac0d7c9982523e880ca0ccd7df3302068d4f6
Pedro Peixoto <phrfpeixoto@gmail.com>
2019-07-14 00:39:00 -0300

    Updating unmarshal and validation methods' signatures to propagate the context until reaching the properties.

--- openapi_core/schema/parameters/models.py
+++ openapi_core/schema/parameters/models.py
@@ -9,6 +9,9 @@ from openapi_core.schema.schemas.enums import SchemaType
 log = logging.getLogger(__name__)
 
 
+_CONTEXT = UnmarshalContext.REQUEST
+
+
 class Parameter(object):
     """Represents an OpenAPI operation Parameter."""
 
