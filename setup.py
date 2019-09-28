4f486c2bc6869f060bc2d0a8b357f3a0b2262a95
p1c2u <maciag.artur@gmail.com>
2019-09-28 21:52:03 +0100

    Setup config refactor

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
+#!/usr/bin/env python
 """OpenAPI core setup module"""
 import os
 import re
 import sys
-
-from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
-
-
-def read_requirements(filename):
-    """Open a requirements file and return list of its lines."""
-    contents = read_file(filename).strip('\n')
-    return contents.split('\n') if contents else []
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+finally:
+    from setuptools.command.test import test as TestCommand
 
 
 def read_file(filename):
@@ -25,32 +25,17 @@ def get_metadata(init_file):
     return dict(re.findall("__([a-z]+)__ = '([^']+)'", init_file))
 
 
-def install_requires():
-    py27 = '_2.7' if sys.version_info < (3,) else ''
-    return read_requirements('requirements{}.txt'.format(py27))
-
-
 class PyTest(TestCommand):
-
     """Command to run unit tests after in-place build."""
 
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = [
-            '-sv',
-            '--flake8',
-            '--junitxml', 'reports/junit.xml',
-            '--cov', 'openapi_core',
-            '--cov-report', 'term-missing',
-            '--cov-report', 'xml:reports/coverage.xml',
-            'tests',
-        ]
-        self.test_suite = True
+        self.pytest_args = []
 
     def run_tests(self):
         # Importing here, `cause outside the eggs aren't loaded.
         import pytest
-        errno = pytest.main(self.test_args)
+        errno = pytest.main(self.pytest_args)
         sys.exit(errno)
 
 
@@ -59,31 +44,12 @@ init_py = read_file(init_path)
 metadata = get_metadata(init_py)
 
 
-setup(
-    name='openapi-core',
-    version=metadata['version'],
-    author=metadata['author'],
-    author_email=metadata['email'],
-    url=metadata['url'],
-    long_description=read_file("README.rst"),
-    packages=find_packages(include=('openapi_core*',)),
-    include_package_data=True,
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        'Intended Audience :: Developers',
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Operating System :: OS Independent",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Software Development :: Libraries',
-    ],
-    install_requires=install_requires(),
-    tests_require=read_requirements('requirements_dev.txt'),
-    extras_require={
-        'flask':  ["werkzeug"],
-    },
-    cmdclass={'test': PyTest},
-    zip_safe=False,
-)
+if __name__ == '__main__':
+    setup(
+        version=metadata['version'],
+        author=metadata['author'],
+        author_email=metadata['email'],
+        url=metadata['url'],
+        cmdclass={'test': PyTest},
+        setup_cfg=True,
+    )
