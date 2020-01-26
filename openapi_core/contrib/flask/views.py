b5aabf213c33f9882950933516890c76a0e13d64
p1c2u <maciag.artur@gmail.com>
2020-01-26 23:29:19 +0000

    FlaskOpenAPIView handler change

--- openapi_core/contrib/flask/views.py
+++ openapi_core/contrib/flask/views.py
@@ -2,6 +2,7 @@
 from flask.views import MethodView
 
 from openapi_core.contrib.flask.decorators import FlaskOpenAPIViewDecorator
+from openapi_core.contrib.flask.handlers import FlaskOpenAPIErrorsHandler
 from openapi_core.validation.request.validators import RequestValidator
 from openapi_core.validation.response.validators import ResponseValidator
 
@@ -9,34 +10,18 @@ from openapi_core.validation.response.validators import ResponseValidator
 class FlaskOpenAPIView(MethodView):
     """Brings OpenAPI specification validation and unmarshalling for views."""
 
-    def __init__(self, request_validator, response_validator):
-        super(MethodView, self).__init__()
-        self.request_validator = request_validator
-        self.response_validator = response_validator
+    openapi_errors_handler = FlaskOpenAPIErrorsHandler
+
+    def __init__(self, spec):
+        super(FlaskOpenAPIView, self).__init__()
+        self.request_validator = RequestValidator(spec)
+        self.response_validator = ResponseValidator(spec)
 
     def dispatch_request(self, *args, **kwargs):
         decorator = FlaskOpenAPIViewDecorator(
             request_validator=self.request_validator,
             response_validator=self.response_validator,
-            openapi_errors_handler=self.handle_openapi_errors,
+            openapi_errors_handler=self.openapi_errors_handler,
         )
         return decorator(super(FlaskOpenAPIView, self).dispatch_request)(
             *args, **kwargs)
-
-    def handle_openapi_errors(self, errors):
-        """Handles OpenAPI request/response errors.
-
-        Should return response object::
-
-            class MyView(FlaskOpenAPIView):
-
-                def handle_openapi_errors(self, errors):
-                    return jsonify({'errors': errors})
-        """
-        raise NotImplementedError
-
-    @classmethod
-    def from_spec(cls, spec):
-        request_validator = RequestValidator(spec)
-        response_validator = ResponseValidator(spec)
-        return cls(request_validator, response_validator)
