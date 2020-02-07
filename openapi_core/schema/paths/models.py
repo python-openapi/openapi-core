aa7ed31acd193b00f8e4348afb662e3114078cb5
Artur Maciag <maciag.artur@gmail.com>
2020-02-07 11:21:11 +0000

    Missing path model fields

--- openapi_core/schema/paths/models.py
+++ openapi_core/schema/paths/models.py
@@ -4,9 +4,15 @@
 class Path(object):
     """Represents an OpenAPI Path."""
 
-    def __init__(self, name, operations, parameters=None):
+    def __init__(
+            self, name, operations,
+            summary=None, description=None, parameters=None, servers=None,
+    ):
         self.name = name
         self.operations = dict(operations)
+        self.summary = summary
+        self.description = description
+        self.servers = servers
         self.parameters = dict(parameters) if parameters else {}
 
     def __getitem__(self, http_method):
