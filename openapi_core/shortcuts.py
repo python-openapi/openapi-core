3ca531a4850ecdfca16c6908ab0a34a00f88a712
Artur Maciag <maciag.artur@gmail.com>
2020-02-18 11:57:15 +0000

    Validators shortcuts fix

--- openapi_core/shortcuts.py
+++ openapi_core/shortcuts.py
@@ -5,10 +5,13 @@ from openapi_core.validation.request.shortcuts import (
     spec_validate_body as validate_body,
     spec_validate_parameters as validate_parameters,
 )
+from openapi_core.validation.request.validators import RequestValidator
 from openapi_core.validation.response.shortcuts import (
     spec_validate_data as validate_data
 )
+from openapi_core.validation.response.validators import ResponseValidator
 
 __all__ = [
     'create_spec', 'validate_body', 'validate_parameters', 'validate_data',
+    'RequestValidator', 'ResponseValidator',
 ]
