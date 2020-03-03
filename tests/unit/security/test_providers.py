05148b8acd34c334d024f22ee10846cd1b431bee
Artur Maciag <maciag.artur@gmail.com>
2020-03-03 12:02:58 +0000

    b64decode issue29427 fix

---
+++ tests/unit/security/test_providers.py
@@ -0,0 +1,33 @@
+import pytest
+
+from openapi_core.schema.security_schemes.models import SecurityScheme
+from openapi_core.security.providers import HttpProvider
+from openapi_core.testing import MockRequest
+
+
+class TestHttpProvider(object):
+
+    @pytest.fixture
+    def scheme(self):
+        return SecurityScheme('http', scheme='bearer')
+
+    @pytest.fixture
+    def provider(self, scheme):
+        return HttpProvider(scheme)
+
+    def test_issue29427(self, provider):
+        """Tests HttpProvider against Issue29427
+        https://bugs.python.org/issue29427
+        """
+        jwt = 'MQ'
+        headers = {
+            'Authorization': 'Bearer {0}'.format(jwt),
+        }
+        request = MockRequest(
+            'http://localhost', 'GET', '/pets',
+            headers=headers,
+        )
+
+        result = provider(request)
+
+        assert result == '1'
