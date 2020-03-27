926a17b9b2f2eaaaf0f875ed0807b0677f37b623
p1c2u <maciag.artur@gmail.com>
2020-03-27 08:58:45 +0000

    Update requests contrib tests

---
+++ tests/integration/contrib/requests/test_requests_responses.py
@@ -0,0 +1,14 @@
+from openapi_core.contrib.requests import RequestsOpenAPIResponse
+
+
+class TestRequestsOpenAPIResponse(object):
+
+    def test_invalid_server(self, response_factory):
+        response = response_factory('Not Found', status_code=404)
+
+        openapi_response = RequestsOpenAPIResponse(response)
+
+        assert openapi_response.data == response.content
+        assert openapi_response.status_code == response.status_code
+        mimetype = response.headers.get('Content-Type')
+        assert openapi_response.mimetype == mimetype
