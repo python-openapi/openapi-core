4c2386f5d426ff74cbc14b14173e51a6235480bf
p1c2u <maciag.artur@gmail.com>
2021-02-01 22:13:19 +0000

    Version 0.13.5

--- openapi_core/__init__.py
+++ openapi_core/__init__.py
@@ -6,7 +6,7 @@ from openapi_core.shortcuts import (
 
 __author__ = 'Artur Maciag'
 __email__ = 'maciag.artur@gmail.com'
-__version__ = '0.13.4'
+__version__ = '0.13.5'
 __url__ = 'https://github.com/p1c2u/openapi-core'
 __license__ = 'BSD 3-Clause License'
 
