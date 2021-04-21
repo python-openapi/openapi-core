bcdbf6eadda7b3b2f13a0f0f3f7e9bb234dd8587
Sergiu-Vlad Bonta <bonat.vlad@gmail.com>
2021-04-21 19:04:05 +0300

    Bugfix #311, openapicore works with falcon 3.0.0

--- openapi_core/contrib/falcon/requests.py
+++ openapi_core/contrib/falcon/requests.py
@@ -11,19 +11,24 @@ from openapi_core.validation.request.datatypes import (
 class FalconOpenAPIRequestFactory:
 
     @classmethod
-    def create(cls, request):
+    def create(cls, request, default_when_empty={}):
         """
         Create OpenAPIRequest from falcon Request and route params.
         """
+        default = default_when_empty
         method = request.method.lower()
 
         # gets deduced by path finder against spec
         path = {}
 
-        # Support falcon-jsonify.
+        # in falcon 3 we must hadle empty media or an exception will be raised
+        if hasattr(request, "get_media"):
+            media = request.get_media(default_when_empty=default)
+        else:
+            media = request.media if request.media else default
+        # # Support falcon-jsonify.
         body = (
-            dumps(request.json) if getattr(request, "json", None)
-            else dumps(request.media)
+            dumps(getattr(request, "json", media))
         )
         mimetype = request.options.default_media_type
         if request.content_type:
