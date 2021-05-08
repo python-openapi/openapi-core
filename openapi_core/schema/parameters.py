d24e57d2c05bcb1765fd1bafb29f98e15c008b76
p1c2u <maciag.artur@gmail.com>
2021-05-08 15:30:33 +0100

    Parameter simple scenarion for any schema type fix

--- openapi_core/schema/parameters.py
+++ openapi_core/schema/parameters.py
@@ -1,11 +1,20 @@
+from __future__ import division
+
+
 def get_aslist(param):
-    return (
-        param.get('schema', None) and
-        param['schema']['type'] in ['array', 'object']
-    )
+    """Checks if parameter is described as list for simpler scenarios"""
+    # if schema is not defined it's a complex scenario
+    if 'schema' not in param:
+        return False
+
+    param_schema = param / 'schema'
+    schema_type = param_schema.getkey('type', 'any')
+    # TODO: resolve for 'any' schema type
+    return schema_type in ['array', 'object']
 
 
 def get_style(param):
+    """Checks parameter style for simpler scenarios"""
     if 'style' in param:
         return param['style']
 
@@ -16,6 +25,7 @@ def get_style(param):
 
 
 def get_explode(param):
+    """Checks parameter explode for simpler scenarios"""
     if 'explode' in param:
         return param['explode']
 
