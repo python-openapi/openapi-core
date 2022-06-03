c1e9f7208731924e38cc1bd7c3760452f3c3a72d
p1c2u <maciag.artur@gmail.com>
2022-06-03 07:41:14 +0100

    Version 0.15.0a1

--- openapi_core/__init__.py
+++ openapi_core/__init__.py
@@ -21,7 +21,7 @@ from openapi_core.validation.response.validators import ResponseValidator
 
 __author__ = "Artur Maciag"
 __email__ = "maciag.artur@gmail.com"
-__version__ = "0.14.2"
+__version__ = "0.15.0a1"
 __url__ = "https://github.com/p1c2u/openapi-core"
 __license__ = "BSD 3-Clause License"
 
