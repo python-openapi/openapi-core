35d15e72f96b0a33d957df002eb8b97c28dd45cc
sean <sean.stewart@mavenclinic.com>
2023-05-10 20:11:51 -0400

    Add support for `aiohttp.web`

---
+++ openapi_core/contrib/aiohttp/__init__.py
@@ -0,0 +1,7 @@
+from openapi_core.contrib.aiohttp.requests import AIOHTTPOpenAPIWebRequest
+from openapi_core.contrib.aiohttp.responses import AIOHTTPOpenAPIWebResponse
+
+__all__ = [
+    "AIOHTTPOpenAPIWebRequest",
+    "AIOHTTPOpenAPIWebResponse",
+]
