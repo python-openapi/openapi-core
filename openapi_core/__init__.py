88c78dc9209b221f572dcb9214b20de2df4773bd
p1c2u <maciag.artur@gmail.com>
2023-11-22 21:44:39 +0000

    Version 0.19.0a1

--- openapi_core/__init__.py
+++ openapi_core/__init__.py
@@ -37,7 +37,7 @@ from openapi_core.validation.response import V31WebhookResponseValidator
 
 __author__ = "Artur Maciag"
 __email__ = "maciag.artur@gmail.com"
-__version__ = "0.18.2"
+__version__ = "0.19.0a1"
 __url__ = "https://github.com/python-openapi/openapi-core"
 __license__ = "BSD 3-Clause License"
 
